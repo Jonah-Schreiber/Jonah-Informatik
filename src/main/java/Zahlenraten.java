e75ebebdd5dc62b512f01fa86c226b0c37f60e0e
Jonah <jonah@gaesdonck>
2022-11-07 14:47:52 +0100

    Zahlen 2

--- src/main/java/Zahlenraten.java
+++ src/main/java/Zahlenraten.java
@@ -11,37 +11,46 @@ public class Zahlenraten {
     Knopf ende;
     Knopf neu;
 
+    int gedachteZahl;
+
 
     public Zahlenraten() {
         fenster = new Fenster("Zahlenraten", 500, 500);
-        titel = new BeschriftungsFeld("Zahlenraten", 45, 30, 110, 30);
+        titel = new BeschriftungsFeld("Zahlenraten", 45, 30, 150, 30);
         titel.setzeSchriftGroesse(20);
-        ein = new BeschriftungsFeld("Welche Zahl ...", 35, 80, 130, 20);
+        ein = new BeschriftungsFeld("Welche Zahl ...", 35, 80, 150, 20);
         eingabe = new ZahlenFeld(80, 100, 40, 30);
         eingabe.setzeSchriftGroesse(20);
         eingabe.setzeFokus();
-        aus = new BeschriftungsFeld("Bewertung", 70, 150, 60, 20);
+        aus = new BeschriftungsFeld("Bewertung", 70, 150, 100, 20);
         kommentar = new TextFeld(20, 170, 160, 30);
         kommentar.setzeBenutzbar(false);
-        ende = new Knopf("Ende", 10, 250, 50, 20);
-        neu = new Knopf("Neu", 140, 250, 50, 20);
-        gedachteZahl = Hilfe.zufall(1, 99);
-        rateZahl = 0;
+        ende = new Knopf("Raten", 10, 250, 150, 20);
+        neu = new Knopf("Start", 160, 250, 150, 20);
+        gedachteZahl = Hilfe.zufall(1, 100);
+        int rateZahl = 0;
 
 
     }
 
-    private int gedachteZahl
+    private void raten() {
 
-    {
-
+        int distance = getAbstand(eingabe.ganzZahl(), gedachteZahl);
+        /*
+        if distance == 0 {
+            System.out.println("Zahl erraten");
+        }
+        //if-Abfragen ob Abstand groß oder klein oder mittel ist
+        */
 
     }
 
-    private int rateZahl
+    private static int getAbstand(int rateZahl, int vergleichsZahl) {
 
-    {
+        int distance = 0;
 
+
+        return distance;
     }
 }
 
