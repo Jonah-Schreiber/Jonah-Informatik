75c0247c74f9cf8f36204584099f6d5a4c23c0c1
Jonah <jonah@gaesdonck>
2022-12-18 19:39:22 +0100

    fertig

--- src/main/java/Arrays/TestArray.java
+++ src/main/java/Arrays/TestArray.java
@@ -1,27 +1,56 @@
 package Arrays;
 
-import basis.*;
+import basis.BeschriftungsFeld;
+import basis.Fenster;
+import basis.Knopf;
+import basis.ZahlenFeld;
+
+import javax.swing.*;
+import java.util.Arrays;
 
 public class TestArray {
-    // Elemente
-    private Fenster fenster;
-    private BeschriftungsFeld anweisung;
-    private Knopf neu, ok, ende;
-    private TextFeld ausgabe1, eingabe1;
+    public Fenster fenster;
+    public String[] tiere;
+    public BeschriftungsFeld b1, b2;
+    public Knopf ok;
 
-    // Variablen
-    private String eingabe, ausgabe;
+    public ZahlenFeld eingabeAnzahl;
 
-    //Konstruktor
-    public TestArray(){
-        //Fenster
-        fenster = new Fenster(1000, 1000);
+    public TestArray() {
+        // Fenster
+        fenster = new Fenster("Arrays", 500, 500);
         //Beschriftungsfeld
-        anweisung = new BeschriftungsFeld("Namen hier eingeben:", 50, 50 , 300, 50);
-        anweisung.setzeSchriftGroesse(30);
-        //Knöpfe
-        ok = new Knopf("OK", 50, 500, 200, 50);
-        ende = new Knopf("Ende", 350, 500, 200, 50);
-        
+        b1 = new BeschriftungsFeld("", 100, 100, 300, 20);
+        b2 = new BeschriftungsFeld("Wie viel Tiere möchten Sie eingeben?:", 100, 250, 300, 20);
+        //Zahlenfeld
+        eingabeAnzahl = new ZahlenFeld(100, 300, 100, 50);
+        //eingabeTiere.setzeTextFeldLauscher();
+        //Knopf
+        ok = new Knopf("OK", 100, 350, 100, 50);
+        ok.setzeKnopfLauscher(knopf -> eingabeTiere());
+
+    }
+
+    public void fuehreAus() {
+        tiere = new String[3];
+
+        tiere[0] = "Hund";
+        tiere[1] = "Katze";
+        tiere[2] = "Maus";
+        System.out.println(tiere[0] + ", " + tiere[1] + ", " + tiere[2]);
+        b1.setzeText(tiere[0] + ", " + tiere[1] + ", " + tiere[2]);
+    }
+
+    public void eingabeTiere() {
+
+        tiere = new String[eingabeAnzahl.ganzZahl()];
+        for (int i = 0; i < eingabeAnzahl.ganzZahl(); i++) {
+            tiere[i] = JOptionPane.showInputDialog("Wähle Tier Nr." + (i + 1));
+            b1.setzeText(Arrays.toString(tiere));
+        }
+
+        //b1.setzeText(tiere[0] + ", " + tiere[1] + ", " + tiere[2]);
     }
 }
+
+
