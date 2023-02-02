6b891477dc18239b8ce5f50393063cd2e8e7bdf7
Jonah <jonah@gaesdonck>
2023-02-02 10:35:51 +0100

    bruch

--- src/main/java/ArrayAnwendung/Verschlüsselung.java
+++ src/main/java/ArrayAnwendung/Verschlüsselung.java
@@ -1,53 +1,120 @@
 package ArrayAnwendung;
 
+import basis.BeschriftungsFeld;
+import basis.Fenster;
+import basis.Knopf;
+import basis.TextFeld;
+
 public class Verschlüsselung {
 
-    public static void main(String[] args) {
-        String plaintext = "Guten Tag!";
-        int key = 3;
 
-        String ciphertext = code(plaintext, key);
-        System.out.println("Klartext: " + plaintext + " ---> " + ciphertext);
+    private Fenster fenster;
+    private BeschriftungsFeld ueberSchriftEing, ueberSchriftAusg;
+    private TextFeld eingabeFeld, ausgabeFeld;
+    private Knopf code, decode;
+    private String eingabe, ausgabe, abc, codierterText, decodierterText;
+    private char aktuellerBuchstabe, neuerBuchstabe;
+    private int key, positionImAlphabet, neuePositionImAlphabet;
 
-        String decoded = decode(ciphertext, key);
-        System.out.println("Geheimtext: " + ciphertext + " ---> " + decoded);
+    public Verschlüsselung() {
+        fenster = new Fenster(500, 150);
+
+        ueberSchriftEing = new BeschriftungsFeld("Hier den Text eingeben", 10, 10, 200, 10);
+        eingabeFeld = new TextFeld(10, 30, 200, 20);
+
+        ueberSchriftAusg = new BeschriftungsFeld("Hier steht der verschlüsselte Text", 220, 10, 300, 10);
+        ausgabeFeld = new TextFeld(220, 30, 200, 20);
+
+        code = new Knopf("Verschlüsseln", 10, 60, 150, 20);
+
+        decode = new Knopf("Entschlüsseln", 220, 60, 150, 20);
+
+
+        abc = "abcdefghijklmnopqrstuvwxyz";
+        key = 3;
+        codierterText = "";
+        decodierterText = "";
+
     }
 
-    /* Klartext mit dem Schluessel verschluesseln */
-    public static String code(String plaintext, int key) {
-        String ciphertext = "";
-        plaintext = plaintext.toLowerCase(); //Alles in Kleinbuchstaben umwandeln
-        for (int i = 0; i < plaintext.length(); i++) {
-            //Buchstaben verschieben und an Geheimtext haengen
-            char next = plaintext.charAt(i);
-            ciphertext = ciphertext + shift(next, key);
+
+        public static void main (String[]args){
+            String plaintext = "Guten Tag!";
+            int key = 3;
+
+            String ciphertext = code(plaintext, key);
+            System.out.println("Klartext: " + plaintext + " ---> " + ciphertext);
+
+            String decoded = decode(ciphertext, key);
+            System.out.println("Geheimtext: " + ciphertext + " ---> " + decoded);
         }
-        return ciphertext;
-    }
 
-    /* Verschiebt den Buchstaben um die angegebene Verschiebung (key)*/
-    public static char shift(char letter, int shift) {
-        //Buchstaben als Zahl behandeln
-        if (letter >= 'a' && letter <= 'z') { //Sonderzeichen nicht veraendern
-            letter += shift;
-            while (letter > 'z') {
-                letter -= 26;
+
+
+// Klartext mit dem Schlüssel verschlüsseln
+        public static String code (String plaintext,int key){
+            String ciphertext = "";
+            plaintext = plaintext.toLowerCase(); //Alles in Kleinbuchstaben umwandeln
+            for (int i = 0; i < plaintext.length(); i++) {
+                //Buchstaben verschieben und an Geheimtext haengen
+                char next = plaintext.charAt(i);
+                ciphertext = ciphertext + shift(next, key);
             }
+            return ciphertext;
         }
-        return letter;
+// Verschiebt den Buchstaben um die angegebene Verschiebung (key)
+        public static char shift ( char letter, int shift){
+            //Buchstaben als Zahl behandeln
+            if (letter >= 'a' && letter <= 'z') { //Sonderzeichen nicht veraendern
+                letter += shift;
+                while (letter > 'z') {
+                    letter -= 26;
+                }
+            }
+            return letter;
+        }
+
+
+
+        // Entschlüsselt den Geheimtext mit dem angegebenen Schlüssel
+        public static String decode (String ciphertext,int key){
+            String decoded = "";
+            ciphertext = ciphertext.toLowerCase(); //Alles in Kleinbuchstaben umwandeln
+            for (int i = 0; i < ciphertext.length(); i++) {
+                //Jeden Buchstaben verschieben und zwar um (26-Schluessel(key)) Stellen
+                //und an entschluesselten Text anhaengen
+                char next = ciphertext.charAt(i);
+                decoded = decoded + shift(next, 26 - key);
+            }
+            return decoded;
+        }
+
+
+
+
+    @Override
+    public void bearbeiteKnopfDruck(Knopf k) {
+        if (k == code) {
+            codierterText = "";
+            eingabe = eingabeFeld.text();
+            this.verschluesseln();
+        }
+
+        if (k == decode) {
+            decodierterText = "";
+            ausgabe = ausgabeFeld.text();
+            this.entschluesseln();
+        }
     }
 
-    /* Entschluesselt den Geheimtext mit dem angegebenen Schluessel */
-    public static String decode(String ciphertext, int key) {
-        String decoded = "";
-        ciphertext = ciphertext.toLowerCase(); //Alles in Kleinbuchstaben umwandeln
-        for (int i = 0; i < ciphertext.length(); i++) {
-            //Jeden Buchstaben verschieben und zwar um (26-Schluessel(key)) Stellen
-            //und an entschluesselten Text anhaengen
-            char next = ciphertext.charAt(i);
-            decoded = decoded + shift(next, 26 - key);
-        }
-        return decoded;
-    }
 }
+
+
+
+
+
+
+
+
+
 
