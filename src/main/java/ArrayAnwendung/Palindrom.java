eecd31459076049071873b2dc500d33b3dd09da4
Jonah <jonah@gaesdonck>
2023-03-09 09:39:46 +0100

    Noch weniger Bock

--- src/main/java/ArrayAnwendung/Palindrom.java
+++ src/main/java/ArrayAnwendung/Palindrom.java
@@ -1,72 +1,50 @@
 package ArrayAnwendung;
 
-import basis.*;
+import basis.BeschriftungsFeld;
+import basis.Fenster;
+import basis.Knopf;
+import basis.TextFeld;
 
 public class Palindrom {
 
 
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private Fenster fenster;
-    private TextFeld eingabeFeld, ausgabeFeld;
-    private Knopf ok, ende;
-    private BeschriftungsFeld ueberschrift, textEingabe;
+    private TextFeld eingabe, ausgabe;
+    private Knopf pruefen;
+    private BeschriftungsFeld ergebnis;
 
-    private int positionImAlpahbet;
-    private char aktBuchstabe, neuBuchstabe;
-    private String eingabe, Eingabe, abc, text, neuertext, a, b;
+    private String eingabeText, ausgabeText;
 
 
     public Palindrom() {
-        // Instanzvariable initialisieren
-        fenster = new Fenster(500, 150);
-
-        ueberschrift = new BeschriftungsFeld("Hier den Text eingeben", 10, 10, 200, 10);
-        textEingabe = new BeschriftungsFeld("Hier steht das Palindrom", 220, 10, 200, 20);
-
-        ok = new Knopf("OK", 10, 60, 150, 20);
-        //ok.setzeKnopfLauscher(this);
-        ende = new Knopf("Ende", 10, 120, 150, 20);
-
-        eingabeFeld = new TextFeld(10, 30, 200, 20);
-        ausgabeFeld = new TextFeld(220, 30, 200, 20);
-
-        abc = "abcdefghijklmnopqrstuvwxyz";
-        text = "";
-        neuertext = "";
-
-
+        fenster = new Fenster((500, 500);
+        eingabe = new TextFeld(50, 50, 400, 30);
+        ausgabe = new TextFeld(50, 100, 400, 30);
+        pruefen = new Knopf("Prüfen", 50, 150, 100, 40);
+        ergebnis = new BeschriftungsFeld("Der Text ist ein Palindrom:", 50, 200, 300, 30);
+        pruefen.setzeKnopfLauscher(knopf -> this.pruefeEingabe());
     }
 
-
-    public void main() {
-        while (!ende.wurdeGedrueckt()) {
-            Hilfe.kurzePause();
-            this.start();
-
+    public void pruefeEingabe() {
+        ausgabeText = "";
+        eingabeText = eingabe.text();
+        //System.out.println(eingabeText);
+        //System.out.println(eingabeText.length());
+        for (int i = eingabeText.length() - 1; i >= 0; i--) ;
+        {
+            ausgabeText += String.valueOf(eingabeText.charAt(i));
         }
-        fenster.gibFrei();
-    }
-
-    public void start() {
-        if (ok.wurdeGedrueckt()) {
-            //System.out.println("test");
-            String a = "eingabe";
-            String b = "Eingabe";
-            if (a.equals(b)) {
-                //int length(a)==
-                System.out.println("sind gleich (Großschreibung berücksichtigt)");
-            } //Ergebnis false
-
-            if (a.equalsIgnoreCase(b)) {
-                System.out.println("sind gleich (Großschreibung nicht berücksichtigt)");
-            } //ergebnis True
-
-
+        //System.out.println(ausgabeText);
+        ausgabe.setzeText(ausgabeText);
+        if (eingabeText.equalsIgnoreCase(ausgabeText)) {
+            ergebnis.setzeText("Der Text ist ein Palindrom: Ja");
+        } else {
+            ergebnis.setzeText("Der Text ist ein Palindrom: Nein");
         }
-
-
     }
 
-    }
+
+}
+
 
 
